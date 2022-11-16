7f09c3b43bb6d51ac3c653b0aae0061c7a5eda0f
devopstraininghub <87629326+devopstraininghub@users.noreply.github.com>
2022-11-16 14:24:26 +0530

    Update WelcomeController.java

--- Application/src/main/java/example/smallest/controllers/WelcomeController.java
+++ Application/src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Congratulation DevOps Engineers ! Cloud and DevOps Engineer jobs are recession proof .. keep working hard and there is light at the end of tunnel.  All the Best for your bright futute !!! From Springboot Java Application"; //"application/json" mean this is a text not a redirect
+		return "Congratulation DevOps Engineers ! Cloud and DevOps Engineer jobs are recession proof.  All the Best for your bright futute !!!  Lets be in touch . Always Feel free to reach me  - 7396627149 / 8374343733  Madhukiran "; //"application/json" mean this is a text not a redirect
 	}
 }
