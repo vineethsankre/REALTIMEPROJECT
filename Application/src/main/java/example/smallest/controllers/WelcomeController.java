acd4cc6be6450a585c4a8c85f3aa5b0188e32f5a
devopstraininghub <87629326+devopstraininghub@users.noreply.github.com>
2025-06-14 15:49:18 +0530

    Update WelcomeController.java

--- Application/src/main/java/example/smallest/controllers/WelcomeController.java
+++ Application/src/main/java/example/smallest/controllers/WelcomeController.java
@@ -8,17 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
+	@RequestMapping(method = RequestMethod.GET, produces = {"text/html"})
 	public @ResponseBody String helloWorld() {
-		
-		//Flux
-		
-		
-		
-		return '<p style="font-size:24px; font-weight:bold;">🎉 Congratulations DevOps Engineers!!! AWS DevOps is a recession-proof IT Career 💼🚀. All the best for your Bright future 🌟🎯</p>';
-// "application/json" means this is a text, not a redirect
 
-		
+		return "<p style=\"font-size:24px; font-weight:bold;\">🎉 Congratulations DevOps Engineers!!! AWS DevOps is a recession-proof IT Career 💼🚀. All the best for your Bright future 🌟🎯</p>";
 
 	}
 }
